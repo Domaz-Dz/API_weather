7857f26ceabd39a74d18023dd9797cd9494dffcb
Domaz-Dz <meabdibouziane@gmail.com>
2024-03-18 23:10:02 -0400

    API weather

---
+++ weather_report.js
@@ -0,0 +1,19 @@
+function showweatherDetails(event) {
+    const city = document.getElementById('city').value;
+    const apiKey = '7ed1d9e54b54846f9a9cd2e68e12b22f'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    }).catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+    event.preventDefault();
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
